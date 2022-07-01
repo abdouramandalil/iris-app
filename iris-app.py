f4917c1048e726d1f5f9c87cff4110687c369b52
Abdouraman Bouba Dalil <abdouramandalil@gmail.com>
2022-07-02 00:17:24 +0100

    iris-app deployment

---
+++ iris-app.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+def main():
+    """ A simple EDA app """
+    st.title("Iris EDA app with streamlit")
+    st.subheader("Streamlit is cool")
+
+if __name__=='__main__':
+    main( )
